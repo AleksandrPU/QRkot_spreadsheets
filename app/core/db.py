97c4dabe0ba4cb6bff4a45b166bf6b25a693f275
Паутов Александр <sh2d000w@gmail.com>
2024-06-17 09:42:48 +0300

    fix: Change " to ' for consistency.

--- app/core/db.py
+++ app/core/db.py
@@ -16,11 +16,11 @@ class PreBase:
 
 Base = declarative_base(cls=PreBase)
 Base.metadata = MetaData(naming_convention={
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_`%(constraint_name)s`',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s'
 })
 
 engine = create_async_engine(settings.database_url)
